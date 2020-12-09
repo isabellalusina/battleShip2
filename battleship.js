4c80da8e6e59dff55e5e5a98e513c3bf38bcc164
isabellalusina <isabella.lusina@github.com>
2020-12-09 14:12:17 +0100

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -59,6 +59,24 @@ var controller = {
     guesses: 0,
 
     processGuess: function(guess) {
+        var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+        if (guess === null || guess.length !== 2) {
+            alert("Ups, proszę wpisać literę i cyfrę.");
+        } else {
+            firstChar = guess.charAt(0);
+            var row = alphabet.indexOf(firstChar);
+            var column = guess.charAt(1);
+            if (isNaN(row) || isNaN(column)) {
+                alert("Ups, to nie są współrzędne!");
+            } else if (row < 0 || row >= model.boardSize ||
+                column < 0 || column >= model.boardSize) {
+                alert("Ups, pole poza planszą!");
+            } else {
+                return row + column;
+            }
+        }
+        return null;
 
     }
 };
